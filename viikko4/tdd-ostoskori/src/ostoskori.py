ac450b61d2dde458557942bed4a5f29e441ad0e6
Nicolas <kivimakinicolas@gmail.com>
2022-11-28 21:09:23 +0200

    5.8 ostoskori palauttaa oikean maaran ostoksia

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,12 +3,12 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.ostokset = []
+        self._ostokset = []
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
         maara = 0
-        for ostos in self.ostokset:
+        for ostos in self._ostokset:
             maara += ostos._lukumaara
         return maara
 
@@ -18,19 +18,19 @@ class Ostoskori:
 
     def hinta(self):
         summa = 0
-        for ostos in self.ostokset:
+        for ostos in self._ostokset:
             summa += ostos.hinta()
         return summa
 
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        for ostos in self.ostokset:
+        for ostos in self._ostokset:
             if ostos.tuote == lisattava:
                 ostos.muuta_lukumaaraa(1)
                 return
         uusi_tuote = Ostos(lisattava)
-        self.ostokset.append(uusi_tuote)
+        self._ostokset.append(uusi_tuote)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -41,6 +41,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
+        return self._ostokset
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -40,3 +40,8 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.leipa)
         self.kori.lisaa_tuote(self.leipa)
         self.assertEqual(self.kori.hinta(), 2 * self.leipa.hinta())
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
+        self.kori.lisaa_tuote(self.maito)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 1)
